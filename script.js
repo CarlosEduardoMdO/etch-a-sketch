0b7d3deb5645aa03ce8e78ba1229cdfb6a064842
Carlos Eduardo <cadumonteiroleet@gmail.com>
2024-11-05 20:37:34 -0400

    Projeto Finalizado com todos os requisitos testados.

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const pixels = 16;
-const grid   = document.getElementById("grid");
+const pixels        = 16;
+const grid          = document.getElementById("grid");
+const buttons       = document.getElementsByClassName("button");
+const eraseButton   = document.getElementById("eraser");
+const customButton  = document.getElementById("custom");
+
+eraseButton.addEventListener("click", () => {descolorirGrid()});
+customButton.addEventListener("click", () => {
+
+    let numPixels = Number(prompt("Insira um número entre 1 e 100 para definir a quantidade de Pixels no quadro."));
+    
+    if(numPixels == 0 || isNaN(numPixels))
+    {
+        return alert("Você não inseriu um valor numérico valido.");
+
+    } else if(numPixels < 1 || numPixels > 100) {
+        
+        return alert("Você inseriu um número fora do intervalo entre 1 e 100.");
+    }
+
+    apagarGrid();
+    gerarPixels(numPixels);
+})
+
+for(const button of buttons)
+{
+    button.addEventListener("click", (button) => {
+        
+        apagarGrid();
+        gerarPixels(button.target.id)}
+    );
+}
 
 gerarPixels(pixels);
 
@@ -16,10 +46,26 @@ function gerarPixels(numPixels)
             const novoPixel = document.createElement("div");
             
             novoPixel.setAttribute("class", "pixel");
+            novoPixel.addEventListener("mouseover", (event) => {event.target.classList.add("permanent-color")});
 
             novaLinha.appendChild(novoPixel);            
         }
 
         grid.appendChild(novaLinha);
+    }
+}
+
+function apagarGrid()
+{
+    grid.replaceChildren();
+}
+
+function descolorirGrid()
+{
+    let currentPixels = document.getElementsByClassName("pixel");
+
+    for(const currentPixel of currentPixels)
+    {
+        currentPixel.classList.remove("permanent-color");
     }
 }
